9a5e80d1c950fe52b920dbb153039fcf2d3a357c
karimatta2 <karimramy2@outlook.com>
2017-11-13 13:13:01 -0500

    argparse added

--- qbasicbackend.py
+++ qbasicbackend.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 import argparse
 def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('masterAccountFile')
+    p.add_argument('transactionFiles', nargs='+')
+    args = p.parse_args()
     t = mergeTransactions()
     newMaster = backend(t, masterAcc)
     createAccListFile(newMaster)
 
 main()
+
+def mergeTransactions():
+
+    return
