6ef058e473a09b65f2411e7dbf555cc64a29d40e
karimatta2 <karimramy2@outlook.com>
2017-11-13 18:08:12 -0500

    creates a merged transaction file

--- qbasicbackend.py
+++ qbasicbackend.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python
 import argparse
+
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('masterAccountFile')
-    p.add_argument('transactionFiles', nargs='+')
+    p.add_argument('transactionFiles',type=file, nargs='+')
     args = p.parse_args()
-    t = mergeTransactions()
-    newMaster = backend(t, masterAcc)
-    createAccListFile(newMaster)
+    master = args.masterAccountFile
+    merged = open("merged.txt", "w")
+    for tFile in args.transactionFiles:
+        line = tFile.readline()
+        while line:
+            if line != "EOS 0000000 000 0000000 ***"+"\n":
+                merged.write(line)
+            line = tFile.readline()
+    merged.close()
+
+    mergeTransactions(merged)
+    '''
+    print("\n")
+    print(master)
+    print("\n")
+    print(transactions)
+    print("\n")
+    print(vars(args))
+    '''
+    #mergeTransactions(transactions)
+    #newMaster = backend(t, masterAcc)
+    #createAccListFile(newMaster)
+
+def mergeTransactions(file):
+    #takes a long transaction file and makes it a list
+
+
+
+
 
 main()
-
-def mergeTransactions():
-
-    return
