be944dc13b38612837f36a8823f72a52dd278785
karimatta2 <karimramy2@outlook.com>
2017-11-19 19:26:43 -0500

    comment about how to test backend

--- backendtest.py
+++ backendtest.py
@@ -7,5 +7,16 @@
 # November 2017
 # This is a python file to test qbasicbackend.py
 
+
+#to test this shit just call backend() with the parameters that you have for your test case
+#so that each test case is one call on backend
+#so if i was testing where transactionList was ["NEW 1234567 000 0000000 Name"] and 
+#masterAccount = [""] then i would call backend(["NEW 1234567 000 0000000 Name"], [""])
+
 from qbasicbackend import backend
-backend()
+#Creation Transactions Testing
+
+
+
+#Withdraw Transactions Testing
+
